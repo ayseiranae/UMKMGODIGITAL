f8ac324620d83c65d85c1720940fdc064a86a48e
Arkadian Abi Destya Utama <abiarkadian12@gmail.com>
2026-07-21 17:10:25 +0700

    fix pertama

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\Admin\CategoryController;
 use App\Http\Controllers\Admin\DashboardController;
 use App\Http\Controllers\Admin\ProductController;
 use App\Http\Controllers\Admin\UmkmController;
+use App\Http\Controllers\Admin\SettingController;
 use App\Http\Controllers\CatalogController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProductCatalogController;
@@ -21,6 +22,7 @@ Route::get('/umkm', [CatalogController::class, 'index'])->name('umkm.index');
 Route::get('/umkm/{slug}', [CatalogController::class, 'show'])->name('umkm.show');
 Route::get('/produk', [ProductCatalogController::class, 'index'])->name('produk.index');
 Route::get('/produk/{umkmSlug}/{productSlug}', [ProductCatalogController::class, 'show'])->name('produk.show');
+Route::get('/katalog', [ProductCatalogController::class, 'index'])->name('katalog.index');
 
 /*
 |--------------------------------------------------------------------------
@@ -33,6 +35,10 @@ Route::middleware(['auth', 'admin'])->prefix('admin')->name('admin.')->group(fun
     Route::resource('umkm', UmkmController::class)->except(['show']);
     Route::resource('produk', ProductController::class)->except(['show']);
     Route::resource('kategori', CategoryController::class)->except(['show']);
+    
+    // Village Settings
+    Route::get('pengaturan', [SettingController::class, 'edit'])->name('pengaturan.edit');
+    Route::put('pengaturan', [SettingController::class, 'update'])->name('pengaturan.update');
 });
 
 /*
